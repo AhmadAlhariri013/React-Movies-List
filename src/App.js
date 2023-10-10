674bbe32e02a11984af5a6b0c90dedf71aef5331
ahmadalhariri013 <ahmadalhariri13@icloud.com>
2023-10-10 18:48:23 +0300

    Developed the basic design of the home page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,39 @@
+import { Header } from "./components/Header";
+import MovieCard from "./components/MovieCard";
+
 function App() {
-  return <div className=""></div>;
+  return (
+    <>
+      {/* This Section For Movies List Page*/}
+      <section className="overflow-hidden min-h-screen pt-3 2xl:px-20 px-7">
+        {/* Start  of the nav header section  */}
+        <Header />
+        {/* End of the nav header section  */}
+
+        {/* Start of movies components list section  */}
+        <section className=" mt-3 mx-auto">
+          <div className="">
+            {/* Start Movies Components */}
+            <div className="grid grid-cols-1 xl-mobile:grid-cols-2  xs-desktop:grid-cols-3 md-desktop:grid-cols-4 xl-desktop:grid-cols-5 2xl-desktop:grid-cols-6   gap-3 w-full">
+              <MovieCard />
+              <MovieCard />
+              <MovieCard />
+              <MovieCard />
+              <MovieCard />
+              <MovieCard />
+              <MovieCard />
+              <MovieCard />
+            </div>
+            {/* Start Movies Components */}
+          </div>
+        </section>
+        {/* End of movies components list section  */}
+
+        {/* The Pagination */}
+        <div></div>
+      </section>
+    </>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,20 @@ module.exports = withMT({
     "path-to-your-node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "md-mobile": "360px",
+        "xl-mobile": "460px",
+        "xs-tablet": "640px",
+        "md-tablet": "768px",
+        "xl-tablet": "1280px",
+        "xs-desktop": "768px",
+        "md-desktop": "1080px",
+        "xl-desktop": "1320px",
+        "2xl-desktop": "1600px",
+        xxl: { max: "1340px" },
+      },
+    },
   },
   plugins: [],
 });
